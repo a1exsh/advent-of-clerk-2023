2c6a3f95e54da64400db614988bfceeccda63f21
a1exsh <alex.shulgin@gmail.com>
2022-12-05 19:50:07 +0100

    start day 05

--- src/advent_of_clerk/day_05.clj
+++ src/advent_of_clerk/day_05.clj
@@ -1,3 +1,18 @@
 ;; # 🎄 Advent of Clerk: Day 5
 (ns advent-of-clerk.day-05
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.string :as string]))
+
+;; ## Parsing the input
+;; Example:
+(def example "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2")
+
+(comment (def input (slurp "input/2022/05")))
