e8d2d2fde5fac6f9d485c88a1e55bf521c3e4ad4
a1exsh <alex.shulgin@gmail.com>
2022-12-06 21:53:59 +0100

    day 06

--- src/advent_of_clerk/day_06.clj
+++ src/advent_of_clerk/day_06.clj
@@ -1,3 +1,24 @@
 ;; # 🎄 Advent of Clerk: Day 6
 (ns advent-of-clerk.day-06
   (:require [nextjournal.clerk :as clerk]))
+
+;; ## The input
+(def example "mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+
+(def input (slurp "input/2022/06"))
+
+;; ## Part I
+;; Life is easy when you know exactly the right function ;)
+(partition 4 1 example)
+
+(defn find-signal [s n]
+  (->> s
+       (partition n 1)
+       (take-while #(->> % distinct count (not= n)))
+       count
+       (+ n)))
+
+(find-signal input 4)
+
+;; ## Part II
+(find-signal input 14)
